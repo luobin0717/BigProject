5df884c4fba9376606d6da50148f21f9d07359db
thisisloves <1543589326@qq.com>
2019-06-23 22:21:01 +0800

    分类静态页面

--- src/views/choose/UI.js
+++ src/views/choose/UI.js
@@ -6,14 +6,119 @@ class App extends React.Component{
     return (
       <div className = "box">
         <header className = "choose-header">
-          <input type="text" placeholder="搜索产品"/>
-          <ul>
-            <li className="active">健康主题</li>
-            <li>类别</li>
-            <li>品牌</li>
-          </ul>
+          <div className="header-left">
+            <img src="http://www.infatuation.top/haitao-img/%E5%88%86%E7%B1%BBimg/%E6%89%AB%E4%B8%80%E6%89%AB.png"/>
+            <p>扫一扫</p>
+          </div>
+          <input type="text" placeholder="雅诗兰黛"/>
+          <img src="http://www.infatuation.top/haitao-img/%E5%88%86%E7%B1%BBimg/%E6%90%9C%E7%B4%A2.png"/>
+          <div className="header-right">
+            <img src="http://www.infatuation.top/haitao-img/%E5%88%86%E7%B1%BBimg/%E6%B6%88%E6%81%AF.png"/>
+            <p>消息</p>
+          </div>
         </header>
-        <div className = "content">选购内容
+        <div className = "choose-content">
+          <div className="content-left">
+            <ul>
+              <li><a className="active">为你推荐</a></li>
+              <li><a>品牌墙</a></li>
+              <li><a>美容彩妆</a></li>
+              <li><a>奶粉止尿裤</a></li>
+              <li><a>母婴专区</a></li>
+              <li><a>营养保健</a></li>
+              <li><a>数码家电</a></li>
+              <li><a>个人洗护</a></li>
+              <li><a>服饰鞋靴</a></li>
+              <li><a>运动户外</a></li>
+              <li><a>手表配饰</a></li>
+              <li><a>家具生活</a></li>
+
+            </ul>
+          </div>
+          <div className="content-right">
+            <div className="content-right-1">
+              <h3>常用分类</h3>
+              <ul>
+                <li>
+                  <img src="http://www.infatuation.top/haitao-img/%E5%88%86%E7%B1%BBimg/%E5%BA%95%E5%A6%86@3x.png"/>
+                  <p>防晒修复</p>
+                </li>
+                <li>
+                  <img src="http://www.infatuation.top/haitao-img/%E5%88%86%E7%B1%BBimg/%E5%BA%95%E5%A6%86@3x.png"/>
+                  <p>防晒修复</p>
+                </li>
+                <li>
+                  <img src="http://www.infatuation.top/haitao-img/%E5%88%86%E7%B1%BBimg/%E5%BA%95%E5%A6%86@3x.png"/>
+                  <p>防晒修复</p>
+                </li>
+                <li>
+                  <img src="http://www.infatuation.top/haitao-img/%E5%88%86%E7%B1%BBimg/%E5%BA%95%E5%A6%86@3x.png"/>
+                  <p>防晒修复</p>
+                </li>
+                <li>
+                  <img src="http://www.infatuation.top/haitao-img/%E5%88%86%E7%B1%BBimg/%E5%BA%95%E5%A6%86@3x.png"/>
+                  <p>防晒修复</p>
+                </li>
+                <li>
+                  <img src="http://www.infatuation.top/haitao-img/%E5%88%86%E7%B1%BBimg/%E5%BA%95%E5%A6%86@3x.png"/>
+                  <p>防晒修复</p>
+                </li>
+                <li>
+                  <img src="http://www.infatuation.top/haitao-img/%E5%88%86%E7%B1%BBimg/%E5%BA%95%E5%A6%86@3x.png"/>
+                  <p>防晒修复</p>
+                </li>
+                <li>
+                  <img src="http://www.infatuation.top/haitao-img/%E5%88%86%E7%B1%BBimg/%E5%BA%95%E5%A6%86@3x.png"/>
+                  <p>防晒修复</p>
+                </li>
+                <li>
+                  <img src="http://www.infatuation.top/haitao-img/%E5%88%86%E7%B1%BBimg/%E5%BA%95%E5%A6%86@3x.png"/>
+                  <p>防晒修复</p>
+                </li>
+              </ul>
+            </div>
+            <div className="content-right-2">
+            <h3>常用分类</h3>
+              <ul>
+                <li>
+                  <img src="http://www.infatuation.top/haitao-img/%E5%88%86%E7%B1%BBimg/%E5%BA%95%E5%A6%86@3x.png"/>
+                  <p>防晒修复</p>
+                </li>
+                <li>
+                  <img src="http://www.infatuation.top/haitao-img/%E5%88%86%E7%B1%BBimg/%E5%BA%95%E5%A6%86@3x.png"/>
+                  <p>防晒修复</p>
+                </li>
+                <li>
+                  <img src="http://www.infatuation.top/haitao-img/%E5%88%86%E7%B1%BBimg/%E5%BA%95%E5%A6%86@3x.png"/>
+                  <p>防晒修复</p>
+                </li>
+                <li>
+                  <img src="http://www.infatuation.top/haitao-img/%E5%88%86%E7%B1%BBimg/%E5%BA%95%E5%A6%86@3x.png"/>
+                  <p>防晒修复</p>
+                </li>
+                <li>
+                  <img src="http://www.infatuation.top/haitao-img/%E5%88%86%E7%B1%BBimg/%E5%BA%95%E5%A6%86@3x.png"/>
+                  <p>防晒修复</p>
+                </li>
+                <li>
+                  <img src="http://www.infatuation.top/haitao-img/%E5%88%86%E7%B1%BBimg/%E5%BA%95%E5%A6%86@3x.png"/>
+                  <p>防晒修复</p>
+                </li>
+                <li>
+                  <img src="http://www.infatuation.top/haitao-img/%E5%88%86%E7%B1%BBimg/%E5%BA%95%E5%A6%86@3x.png"/>
+                  <p>防晒修复</p>
+                </li>
+                <li>
+                  <img src="http://www.infatuation.top/haitao-img/%E5%88%86%E7%B1%BBimg/%E5%BA%95%E5%A6%86@3x.png"/>
+                  <p>防晒修复</p>
+                </li>
+                <li>
+                  <img src="http://www.infatuation.top/haitao-img/%E5%88%86%E7%B1%BBimg/%E5%BA%95%E5%A6%86@3x.png"/>
+                  <p>防晒修复</p>
+                </li>
+              </ul>
+            </div>
+            </div>
         </div>
       </div>
     )
