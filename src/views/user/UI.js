e4e31abc4df9d8543a8c5ba4b34acee53969c193
tianguoyuan <603660895@qq.com>
2019-06-24 14:02:43 +0800

    6-24

--- src/views/user/UI.js
+++ src/views/user/UI.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Carousel, WingBlank } from 'antd-mobile';
+
 import '@/css/user_home.scss';
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    
+    this.state = {
+      data: ['1', '2', '3'],
+      imgHeight: 176,
+    }
   }
   // 
-  componentDidMount(){
-    
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        data: ['banner1@3x.png', 'banner2@3x.png', 'banner3@3x.png','banner4@3x.png'],
+      });
+    }, 100);
   }
   // 回到首页
-  btnHome(){this.props.history.push('/home');}
+  btnHome() { this.props.history.push('/home'); }
   // 
   render() {
     return (
@@ -19,12 +27,36 @@ class App extends React.Component {
         <header className="header">
           {/* 回首页 */}
           <button className="btnHome" onClick={this.btnHome.bind(this)}>回首页</button>
-          <img className="logo" src='http://a1.qpic.cn/psb?/V14cuMaT2oPSVa/yej5aqg*rd5Gb5G8SybF*vuL7No6BxpPm8tnJfdKKwc!/m/dDQBAAAAAAAAnull&bo=MgEyATIBMgEDCSw!&rf=photolist&t=5' />
+          <img className="logo" src='http://www.infatuation.top/haitao-img/%E7%99%BB%E5%BD%95%E9%A1%B5%E9%9D%A2img/logo@3x.png' alt="logo" />
         </header>
         <div className="content">
           {/* Banner */}
           <div className="banner">
-            <img src="http://a1.qpic.cn/psb?/V14cuMaT2oPSVa/B*kXAHV16QjvpC3wnmNpQQJp8PDWJ20dniOuFeYl5.I!/m/dMAAAAAAAAAAnull&bo=ZQQTAmUEEwIDCSw!&rf=photolist&t=5" />
+            <Carousel
+              autoplay={true}
+              infinite
+              beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
+              afterChange={index => console.log('slide to', index)}
+            >
+              {this.state.data.map(val => (
+                <a
+                  key={val}
+                  href="http://www.alipay.com"
+                  style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
+                >
+                  <img
+                    src={`http://www.infatuation.top/haitao-img/%E7%99%BB%E5%BD%95%E9%A1%B5%E9%9D%A2img/${val}`}
+                    alt=""
+                    style={{ width: '100%', verticalAlign: 'top' }}
+                    onLoad={() => {
+                      // fire window resize event to change height
+                      window.dispatchEvent(new Event('resize'));
+                      this.setState({ imgHeight: 'auto' });
+                    }}
+                  />
+                </a>
+              ))}
+            </Carousel>
           </div>
 
           {/* Login  register */}
@@ -37,8 +69,8 @@ class App extends React.Component {
         <footer className="oauth">
           <h3 className="title">一键登录</h3>
           <div class="twoLogin">
-            <img src="http://a4.qpic.cn/psb?/V14cuMaT2oPSVa/*JliDKdDPn7mlDHT*EMWszzc*9pkp8IkeWgv2tXUHKU!/m/dFMBAAAAAAAAnull&bo=twC3ALcAtwADCSw!&rf=photolist&t=5" />
-            <img src="http://a1.qpic.cn/psb?/V14cuMaT2oPSVa/6OvxY7XEJ*YW58ML.BJh8vrqmC1.aF9xpBGaYMKUXK8!/m/dFQBAAAAAAAAnull&bo=twC3ALcAtwADCSw!&rf=photolist&t=5" />
+            <img src="http://www.infatuation.top/haitao-img/%E7%99%BB%E5%BD%95%E9%A1%B5%E9%9D%A2img/QQ%E7%99%BB%E5%BD%95@3x.png" />
+            <img src="http://www.infatuation.top/haitao-img/%E7%99%BB%E5%BD%95%E9%A1%B5%E9%9D%A2img/%E5%BE%AE%E4%BF%A1@3x.png" />
           </div>
         </footer>
       </div>
