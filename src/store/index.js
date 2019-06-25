4ac661783735194203b4884914158fc977c3776e
thisisloves <1543589326@qq.com>
2019-06-25 08:36:39 +0800

    删除

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { createStore, combineReducers } from 'redux';
-import home from '@/views/home/store';
-const reducer = combineReducers({
-  home
-});
-
-const store = createStore(reducer);
-
-export default store;
--- src/views/find/index.js
+++
@@ -1,6 +0,0 @@
-import { connect } from 'react-redux';
-import UI from './UI';
-
-const App = connect()(UI)
-
-export default App;
